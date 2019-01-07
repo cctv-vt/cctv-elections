6004f520e681090c81ecba088119921e1ca26958
Solomon Zeitlyn <szeitlyn@cctv.org>
2019-01-07 11:29:11 -0500

    cleaned code

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,11 @@
 var refresh_rate = 20000;
-
+//var api_endpoint = "https://elections.cctv.org/elections/index.php?f=results";
+var api_endpoint = "./js/election-results.json"
 //Vue Creation
 var app = new Vue({
     el: '#app',
     data: {
-        districts: [
-          {
-            show:true
-          },
-          {
-            show:true
-          },
-          {
-            show:true
-          },
-          {
-            show:true
-          }
-        ]
+        districts: []
     },
     mounted () {
       this.getResults();
@@ -29,7 +17,7 @@ var app = new Vue({
     //TODO:Todo
     methods: {
         getResults: function() {
-            axios.get("https://elections.cctv.org/elections/index.php?f=results")
+            axios.get(api_endpoint)
             .then(function(response){
                 app.districts = response.data.districts;
                 console.log(response.data.districts);
