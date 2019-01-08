8b162ee81d63ea3c7c4e0197364868da4469ceed
Solomon Zeitlyn <szeitlyn@cctv.org>
2019-01-07 20:28:08 -0500

    added multi district functionality

--- js/app.js
+++ js/app.js
@@ -1,33 +1,53 @@
-var refresh_rate = 20000;
+let refresh_rate = 20000;
 //var api_endpoint = "https://elections.cctv.org/elections/index.php?f=results";
-var api_endpoint = "./js/election-results.json"
+let api_endpoint = "./js/election-results.json"
 //Vue Creation
-var app = new Vue({
-    el: '#app',
-    data: {
-        districts: []
-    },
-    mounted () {
-      this.getResults();
+const getSum = function (obj) {
+  let sum = 0;
+  for (i in obj) {
+    sum += obj[i];
+  }
+  return sum;
+};
 
-      setInterval(function () {
-        this.getResults();
-      }.bind(this), refresh_rate);
+var app = new Vue({
+  el: '#app',
+  data: {
+    districts: [],
+    districtShow: [{
+      show: true
+    }]
+  },
+  mounted() {
+    this.getResults();
+    setInterval(function () {
+      this.getResults();
+    }.bind(this), refresh_rate);
+  },
+  //TODO:Todo
+  methods: {
+    getResults: function () {
+      axios.get(api_endpoint)
+        .then(function (response) {
+          app.districts = response.data.districts;
+          //console.log(response.data.districts);
+        }).catch(error => {
+          console.log(error);
+        });
     },
-    //TODO:Todo
-    methods: {
-        getResults: function() {
-            axios.get(api_endpoint)
-            .then(function(response){
-                app.districts = response.data.districts;
-                console.log(response.data.districts);
-            }).catch(error => { console.log(error); });
-          },
-        sortVotes: function(arr) {
-            // Set slice() to avoid to generate an infinite loop!
-            return arr.slice().sort(function(a, b) {
-              return b.votes - a.votes;
-            });
-          },
+    sortVotes: function (arr) {
+      // Set slice() to avoid to generate an infinite loop!
+      return arr.slice().sort(function (a, b) {
+        console.log(getSum(b.votes) - getSum(a.votes));
+        return getSum(b.votes) - getSum(a.votes);
+      });
+    },
+    getSum: function (obj) {
+      let sum = 0;
+      for (i in obj) {
+        sum += obj[i];
+      }
+      return sum;
     }
+  }
 });
