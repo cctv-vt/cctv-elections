34dacbeaa9f7beb19e3d23a5a9bb989108947137
Donkey Kong <szeitlyn@cctv.org>
2019-01-23 11:32:38 -0500

    fixed css and added subresults

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 let refresh_rate = 20000;
 //var api_endpoint = "https://elections.cctv.org/elections/index.php?f=results";
 let api_endpoint = "./js/election-results.json"
-//Vue Creation
+
+//Local Functions
+//getSum is used to get and sum the subresults to form the total number of votes
 const getSum = (obj) => {
   let sum = 0;
   for (i in obj) {
@@ -10,6 +12,12 @@ const getSum = (obj) => {
   return sum;
 };
 
+//subToggle is used to toggle subresults
+const subToggle = (event) => {
+  
+}
+
+//Vue Creation
 var app = new Vue({
   el: '#app',
   data: {
