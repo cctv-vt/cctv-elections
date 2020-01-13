aa904827a12b657c06842f833f39f7e64f3f9a81
Solomon Zeitlyn <szeitlyn@cctv.org>
2020-01-13 17:42:25 -0500

    added simple admin editing features

--- dist/admin/js/main.js
+++ dist/admin/js/main.js
@@ -1,5 +1,6 @@
 // Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
+var db = new firebase.database();
 
 var uiConfig = {
     callbacks: {
@@ -30,14 +31,64 @@ var uiConfig = {
 ui.start('#firebaseui-auth-container', uiConfig);
 
 
-var updateResults = firebase.functions().httpsCallable('updateDatabase');
+// var updateResults = firebase.functions().httpsCallable('helloWorld');
 
-window.addEventListener('load', () => {
-    document.getElementById('update-button').addEventListener('click', (event) => {
-        document.getElementById('update-button').disabled = true;
-        updateResults({ event:"tmd19" }).then((result) => {
-            console.log(result.data)
-            document.getElementById('update-button').disabled = false;
+
+// window.addEventListener('load', () => {
+
+//     document.getElementById('update-button').addEventListener('click', (event) => {
+//         document.getElementById('update-button').disabled = true;
+//         updateResults({ event: "tmd19" }).then((result) => {
+//             console.log(result.data)
+//             document.getElementById('update-button').disabled = false;
+//         })
+
+//     })
+// })
+
+app = new Vue({
+    el: "#app",
+    data: {
+        activeTemplate: 'test',
+        activeDistrict: 0,
+        templates: {
+            test: {
+                districts: [
+                    {
+                        title: "Hello",
+                        
+                    }
+                ]
+            }
+        }
+    },
+    mounted() {
+        this.getTemplate();
+    },
+    methods: {
+        getTemplate() {
+            templatesRef = db.ref('templates/');
+            templatesRef.once('value', function(snapshot) {
+                app.templates = snapshot.val()
+            })
+        },
+        saveTemplate() {
+            activeDistrictRef = db.ref('templates/' + app.activeTemplate + '/districts/' + app.activeDistrict + '/')
+            activeDistrictRef.set(app.templates[app.activeTemplate].districts[app.activeDistrict])
+
+        },
+        updateResults() {
+            document.getElementById('update-button').disabled = true;
+            firebase.functions().httpsCallable('updateDatabase')({ event: app.activeTemplate }).then((result) => {
+                console.log(result.data)
+                document.getElementById('update-button').disabled = false;
         })
-    })
+        },
+        eventChange(key) {
+            app.activeTemplate = key;
+        },
+        districtChange(key) {
+            app.activeDistrict = key;
+        },
+    }
 })
