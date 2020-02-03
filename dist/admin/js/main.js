b4af6468a3b47c5aab69d4a06299c966541185e5
Gobolin <solomon.zeitlyn@gmail.com>
2020-02-03 16:39:00 -0500

    added control panel page for post auth environment

--- dist/admin/js/main.js
+++ dist/admin/js/main.js
@@ -2,74 +2,31 @@
 // var ui = new firebaseui.auth.AuthUI(firebase.auth());
 var db = new firebase.database();
 
-// var uiConfig = {
-//     callbacks: {
-//         signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-//             // User successfully signed in.
-//             // Return type determines whether we continue the redirect automatically
-//             // or whether we leave that to developer to handle.
-//             console.log(authResult)
-//             document.getElementById('control-panel').style.display = 'block';
-//             firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
-//             return false;
-//         },
-//         uiShown: function () {
-//             // The widget is rendered.
-//             // Hide the loader.
-//             document.getElementById('loader').style.display = 'none';
-//         }
-//     },
-//     signInFlow: 'popup',
-//     signInOptions: [
-//         {
-//             provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-//             requireDisplayName: false
-//         }
-//     ]
-// }
+var url = new URLSearchParams(window.location.search);
+var ev = url.get("ev") || "tmd19"
+var d = url.get("d") || 0
+var mode = url.get("m") || "live"
 
-// ui.start('#firebaseui-auth-container', uiConfig);
-
-
-// var updateResults = firebase.functions().httpsCallable('helloWorld');
 firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
 
-
-window.addEventListener('load', () => {
-    document.getElementById('auth-panel').addEventListener('submit', (event) => {
-        var email = document.getElementById('email')
-        var password = document.getElementById('password')
-        firebase.auth().signInWithEmailAndPassword(email.value, password.value).catch( (error) => {
-            console.log(error.code, error.message)
-            // email.value = ''
-            password.value = ''
-        })
-        event.preventDefault()
-    })
-    document.getElementById('logout-button').addEventListener('click', () => {
-        firebase.auth().signOut().then(console.log("Signed Out"))
-    })
-})
-
-firebase.auth().onAuthStateChanged(function(user) {
+firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
-      console.log('Signed in as', user.displayName)
-      document.getElementById('auth-panel').style.display = "none"
-      document.getElementById('control-panel').style.display = 'block';
+        console.log('Signed in as', user.displayName)
+        //   document.getElementById('auth-panel').style.display = "none"
+        document.getElementById('control-panel').style.display = 'block';
     } else {
-        document.getElementById('auth-panel').style.display = "block"
-        document.getElementById('control-panel').style.display = 'none';
+        window.location = "./index.html"
     }
-  });
-  
+});
+
 
 app = new Vue({
     el: "#app",
     data: {
-        loaded:false,
-        activeTemplate: 'tmd19',
-        activeDistrict: 0
-        
+        loaded: false,
+        mode: mode,
+        activeTemplate: ev,
+        activeDistrict: d
     },
     mounted() {
         this.getTemplate();
@@ -77,37 +34,121 @@ app = new Vue({
     methods: {
         getTemplate() {
             templatesRef = db.ref('templates/');
-            templatesRef.once('value', function(snapshot) {
+            templatesRef.once('value', function (snapshot) {
                 app.templates = snapshot.val()
+                app.originalTemplates = snapshot.val()
                 app.loaded = true
             })
         },
         saveTemplate() {
             activeDistrictRef = db.ref('templates/' + app.activeTemplate + '/districts/' + app.activeDistrict + '/')
             activeDistrictRef.set(app.templates[app.activeTemplate].districts[app.activeDistrict])
-
+            this.$forceUpdate()
+        },
+        saveEvSettings() {
+            
         },
         updateResults() {
             document.getElementById('update-button').disabled = true;
+            document.getElementById('update-button').classList.toggle('is-loading')
             firebase.functions().httpsCallable('updateDatabase')({ event: app.activeTemplate }).then((result) => {
                 console.log(result.data)
                 document.getElementById('update-button').disabled = false;
-        })
+                document.getElementById('update-button').classList.toggle('is-loading')
+            })
         },
         eventChange(key, ev) {
-            app.activeTemplate = key;
+            // app.activeTemplate = key;
+            window.location.href = "/controlpanel.html?ev=" + key + "&m=" + mode + "&d=" + 0
         },
         districtChange(key, ev) {
-            app.activeDistrict = key;
+            // app.activeDistrict = key;
+            window.location.href = "/controlpanel.html?ev=" + app.activeTemplate + "&m=template" + "&d=" + key
+        },
+        toggleModal(e) {
+            m = document.getElementById(e).classList.toggle("is-active")
         },
         addVoteEntry(election) {
+            console.log(election)
             for (result of election.results) {
-                result.votes.push({
-                    value: result.votes[0].value,
-                    title: result.votes[0].title
-                })
+                console.log(result.votes.length)
+                if (result.votes.length > 0) {
+                    result.votes.push(JSON.parse(JSON.stringify(result.votes[0])))
+                } else {
+                    result.votes[0] = {
+                        value: "a1",
+                        title: "Default"
+                    }
+                }
             }
             this.$forceUpdate()
+        },
+        removeVoteEntry(election, key) {
+            if(window.confirm("Are you sure you want to delete the spreadsheet: " + election.results[0].votes[key].title + "?")) {
+                console.log(election)
+                for (result of election.results) {
+                    console.log(result)
+                    if (result.votes.length > 1) result.votes.splice(key, 1)
+                }
+                this.$forceUpdate()
+            }
+            
+        },
+        addResult(election, key) {
+            election.results.push(JSON.parse(JSON.stringify(election.results[election.results.length - 1])))
+            this.$forceUpdate()
+        },
+        removeResult(election, key) {
+            if(window.confirm("Are you sure you want to delete the result: "+ election.results[key].name+"?")) {
+                console.log(key)
+                election.results.splice(key, 1)
+                this.$forceUpdate()
+            }
+            
+        },
+        addElection(district) {
+            console.log(district)
+            if (district.elections) {
+                district.elections.push({
+                    title: "New Election",
+                    results: [
+                        {
+                            name: "New Result",
+                            votes: [
+                                {
+                                    title: "Vote Field",
+                                    value: "a1"
+                                }
+                            ]
+                        }
+                    ]
+                })
+            } else {
+                district.elections = [{
+                    title: "New Election",
+                    results: [
+                        {
+                            name: "New Result",
+                            votes: [
+                                {
+                                    title: "Vote Field",
+                                    value: "a1"
+                                }
+                            ]
+                        }
+                    ]
+                }
+                ]
+            }
+            
+            this.$forceUpdate()
+        },
+        removeElection(district, key) {
+            if(window.confirm("Are you sure you want to delete the ballot item: "+district.elections[key].title+"?")) {
+                district.elections.splice(key,1)
+                this.$forceUpdate()
+            }
+            
         }
     }
 })
